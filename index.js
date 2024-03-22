d9fe930bebae5edd9556c7ea5783afd498f667fa
andrew-app <andrew.appuhamy@gmail.com>
2024-03-22 13:11:26 +1100

    use switch statement to handle conditionals and use past thirty min as default return

--- index.js
+++ index.js
@@ -2,39 +2,57 @@
 function convertTimeToWords(time) {
   // TODO: real code goes here!
   
-  const timeArray = time.split(':');
-
   const timeMap = {
-    '1':'one',
-    '2':'two',
-    '3':'three',
-    '4':'four',
-    '5':'five',
-    '6':'six',
-    '7':'seven',
-    '8':'eight',
-    '9':'nine',
+    '0': 'twelve',
+    '1': 'one',
+    '2': 'two',
+    '3': 'three',
+    '4': 'four',
+    '5': 'five',
+    '6': 'six',
+    '7': 'seven',
+    '8': 'eight',
+    '9': 'nine',
     '10': 'ten',
     '11': 'eleven',
     '12': 'twelve',
-    '13': 'thirteen'
+    '13': 'one',
+    '14': 'two',
+    '15': 'three',
+    '16': 'four',
+    '17': 'five',
+    '18': 'six',
+    '19': 'seven',
+    '20': 'eight',
+    '21': 'nine',
+    '22': 'ten',
+    '23': 'eleven',
+    '30': 'thirty',
+    '45': 'quarter',
+    '00': 'o\' clock'
   }
+
+  const timeArray = time.split(':');
 
   if (timeArray[0] === '0' && timeArray[1] === '00') {
     return 'midnight';
   }
 
-  if (timeArray[1] === '30') {
-    return `half past ${timeMap[timeArray[0]]}`
+  switch(timeArray[1]) {
+    case '30':
+      return `half past ${timeMap[timeArray[0]]}`;
+
+    case '45':
+      return `quarter to ${timeMap[parseInt(timeArray[0]) + 1]}`;
+
+    case '00':
+      return `${timeMap[timeArray[0]]} ${timeMap[timeArray[1]]}`;
+    
+    case Number(timeArray[1]) < 30:
+      return `${timeMap[timeArray[1]]} past ${timeMap[timeArray[0]]}`;
   }
 
-  if (timeArray[1] === '45') {
-    return `quarter to ${timeMap[timeArray[0]]}`
-  }
-
-  if (Number(timeArray[1]) < 30) {
-    return `${timeMap[time[0]]} past ${timeArray}`
-  }
+  return `${timeMap[60 - Number(timeArray[1])]} to ${timeMap[parseInt(timeArray[0]) + 1]}`;
 
 }
 
